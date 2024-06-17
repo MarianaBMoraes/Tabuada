c576cf2525d08e04a2e8754dc01c2a13d0bfc213
Mariana <marianabmoraes.ef@gmail.com>
2024-06-17 11:01:12 -0300

    feat: second commit

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ let numero = 0;
 console.log("Digite um número para gerar a tabuada de 1 a 10:");
 
 process.stdin.on("data", function (data) {
-  entrada = data.toString().trim().toLowerCase();
-  numero = entrada;
+  numero = data.toString().trim().toLowerCase();
   for (let i = 1; i <= 10; i++) {
-    console.log(numero * i);
+    console.log(numero + "x" + i + "=" + (numero * i));
   }
   process.exit();
 });
